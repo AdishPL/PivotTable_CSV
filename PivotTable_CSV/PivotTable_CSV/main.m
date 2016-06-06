6203d5a0e8188ee707c9ea49fe252d42b6a9379b
Adrian Kaczmarek <adish@MacBook-Pro-Adrian.local>
2016-06-06 21:06:55 +0200

    Summed values + comma string number to float

--- PivotTable_CSV/PivotTable_CSV/main.m
+++ PivotTable_CSV/PivotTable_CSV/main.m
@@ -12,6 +12,9 @@ int main(int argc, const char * argv[]) {
     @autoreleasepool {
         NSMutableArray *colA = [NSMutableArray array];
         NSMutableArray *colB = [NSMutableArray array];
+        
+        NSMutableDictionary *dictionaryOfRelations = [NSMutableDictionary new];
+        
         NSString* fileName = [[NSBundle mainBundle] pathForResource:@"example" ofType:@"csv"];
         NSError *error;
         NSString *fileContents = [NSString stringWithContentsOfFile:fileName encoding:NSUTF8StringEncoding error:&error];
@@ -19,14 +22,61 @@ int main(int argc, const char * argv[]) {
         if (error) {
             NSLog(@"Error reading file: %@", error.localizedDescription);
         } else {
-        NSArray* rows = [fileContents componentsSeparatedByString:@"\n"];
-        for (NSString *row in rows){
-            NSArray* columns = [row componentsSeparatedByString:@";"];
-            [colA addObject:columns[0]];
-            [colB addObject:columns[1]];
-        }
+            NSArray* rows = [fileContents componentsSeparatedByString:@"\n"];
+            
+            for (NSString *row in rows){
+                NSArray* columns = [row componentsSeparatedByString:@";"];
+                
+                id indexColumn = columns[0];
+                id relationColumn = columns[1];
+                id valueColumn = columns[2];
+                
+                if ([dictionaryOfRelations objectForKey:indexColumn] == nil) {
+                    [dictionaryOfRelations setObject:[NSMutableDictionary new] forKey:indexColumn];
+                }
+                
+                NSMutableDictionary *currentEntries = [dictionaryOfRelations objectForKey:indexColumn];
+                
+//                NSMutableDictionary *flexibleMapping = [NSMutableDictionary new];
+//                
+//                for (NSMutableDictionary *dict in currentEntries) {
+//                    if ([dict objectForKey:relationColumn] == nil) {
+//                        [dict setObject:[NSMutableDictionary new] forKey:relationColumn];
+//                    }
+//                    
+//                    id actuallValue = [dict objectForKey:relationColumn];
+//                    id newValue = [NSString stringWithFormat:@"%@ + %@",actuallValue,valueColumn];
+//                    
+//                    NSMutableDictionary *dictionaryRelationValue = [NSMutableDictionary dictionaryWithObject:newValue forKey:relationColumn];
+//                    
+//                    [flexibleMapping setObject:dictionaryOfRelations forKey:indexColumn];
+//
+//                }
+                float stringToFloat = [[valueColumn stringByReplacingOccurrencesOfString:@"," withString:@"."] floatValue];
+
+                id currentValueForRelation = [currentEntries objectForKey:relationColumn];
+
+                float summedValues = [currentValueForRelation floatValue] + stringToFloat;
+                
+                if (currentValueForRelation == nil) {
+                    summedValues = stringToFloat;
+                }
+                  
+                [currentEntries setObject:@(summedValues) forKey:relationColumn];
+                
+                [dictionaryOfRelations setObject:currentEntries forKey:indexColumn];
+            }
         
-        NSLog(@"ColA %@",colA);
+        NSLog(@"ditionary %@",dictionaryOfRelations);
+            
+            NSMutableDictionary *summedDictionary = [NSMutableDictionary new];
+            
+            for (id newEntry in dictionaryOfRelations) {
+                for (id values in [dictionaryOfRelations objectForKey:newEntry]) {
+                    
+                    NSLog(@"values - %@",values);
+                }
+            }
     
         }
         
